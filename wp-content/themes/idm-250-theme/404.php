2f0c5c4e062b718d954fdb15ce9da39a2481a90c
leelaur <laurenlee552@yahoo.com>
2023-02-27 20:07:39 -0500

    class 2/27

---
+++ wp-content/themes/idm-250-theme/404.php
@@ -0,0 +1,7 @@
+<?php get_header();?>
+<!-- If you want to change your header for 404 page 
+you can do '404' and it will look for the header-404.php file -->
+
+<h1> 404</h1>
+
+<?php get_footer(); ?>
