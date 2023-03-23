b7092ff2044ffb394a068a24daf9698d418a7d3f
leelaur <laurenlee552@yahoo.com>
2023-03-23 13:33:41 -0400

    single defaiult

--- wp-content/themes/idm-250-theme/components/hero-excerpt.php
+++ wp-content/themes/idm-250-theme/components/hero-excerpt.php
@@ -1,5 +1,6 @@
 <div class="hero">
     <div class="hero-content">
     <h1 class="title"><?php echo get_the_title(); ?> </h1>
+        <p> <?php echo get_the_excerpt(); ?> <p>
     </div>
 </div>
--- wp-content/themes/idm-250-theme/single.php
+++ wp-content/themes/idm-250-theme/single.php
@@ -1,71 +1,15 @@
 <?php get_header(); ?>
 <?php get_template_part('components/hero');?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php get_template_part('components/content');?>
-</article>
-
-<div class="categories">
-    <h4> 
-
-        Categories for this post: 
-    </h4> 
-        <p class="category-names"> <?php
-    $currentPostId = get_the_id();
-
-    $terms = get_the_terms($currentPostId, 'category');
-
-    foreach ($terms as $term) {
-        echo $term->name, '';
-
-    }
-
-    ?>
-    </p>
+<div class="content">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php get_template_part('components/content');?>
+    </article>
 </div>
 
 
+
 <br>
 <br>
-<section>
-    <h1> SINGLWE</h1>
-    <div>
-        <!-- <img src="dist/images/family-and-friends-book2.jpg" alt="Family and Friends book"/> -->
-    </div>
-    <div>
-        <h3> Course Description </h3>
-        This CPR certification course is designed to provide participants with the knowledge and skills needed to perform CPR (Cardiopulmonary Resuscitation) in emergency situations. CPR is a critical technique that can help save the life of someone who is not breathing or whose heart has stopped.
-    </div>
-</section>
-<section>
-    <h3> Course Content </h3>
-    <ul>
-        <li class="list">List one</li>
-        <li class="list">List two</li>
-        <li class="list">List three</li>
-        <li class="list">List four</li>
-    </ul>
-</section>
-<section>
-    <h3> How it works </h3> 
-    <div class="two-part">
-        <div>
-            <h5> Part 1:</h5>
-            <p>
-                Select and complete the AHA   Heartsaver®  Online learning module. It  has a personalized adaptive algorithm and is self-directed learning. After completion of the online course, print Certificate of Online Course Completion and   email us  a copy. You can proceed to Part 2 Skills Session, a hands-on skills practice/test appointment.
-            </p>
-        </div>
-        <div>
-            <h5> Part 2:</h5>
-            <p>
-                Select and complete the AHA   Heartsaver®  Online learning module. It  has a personalized adaptive algorithm and is self-directed learning. After completion of the online course, print Certificate of Online Course Completion and   email us  a copy. You can proceed to Part 2 Skills Session, a hands-on skills practice/test appointment.
-            </p>
-        </div>
-    </div>
-</section>
-<section>
-    <h3> Course Fee </h3>
-    <p> $15 </p>
-</section>
 
 
 <div class="bottom-space"></div>
