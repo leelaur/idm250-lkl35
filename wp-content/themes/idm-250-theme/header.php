d557887d5477a8a601bccb8b268d5838d417483a
leelaur <laurenlee552@yahoo.com>
2023-03-24 02:08:56 -0400

    favicon?

--- wp-content/themes/idm-250-theme/header.php
+++ wp-content/themes/idm-250-theme/header.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php wp_title(''); ?> | Excel CPR</title>
-
-    <link rel="stylesheet" href="/wp-content/themes/idm-250-theme/dist/styles/main.css">
-
+    <title> <?php echo get_the_title(); ?> | Excel CPR</title>
+    <link rel="icon" type="image/x-icon" href="dist/images/favicon.ico">
    <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
