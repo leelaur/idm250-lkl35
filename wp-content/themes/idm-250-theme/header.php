54cc6b36df89bbe321ce7b7da787faa2c3e157ba
leelaur <laurenlee552@yahoo.com>
2023-03-24 02:14:53 -0400

    Update header.php

--- wp-content/themes/idm-250-theme/header.php
+++ wp-content/themes/idm-250-theme/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo get_the_title(); ?> | Excel CPR</title>
-    <link rel="icon" type="image/x-icon" href="dist/images/favicon.ico">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico">
    <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
